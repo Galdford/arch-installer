764ffcc703c2c8425481db0311a8e13aa607aef4
Galdford <galdford.verdugo@gmail.com>
2021-06-21 16:58:10 -0400

    Add files via upload

--- simple_reflector.sh
+++ simple_reflector.sh
@@ -2,4 +2,4 @@
 echo
 echo "Simple Reflector by SimplyArch"
 echo
-reflector --verbose --protocol http --protocol https --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose --country 'Chile' -l 5 --sort rate --save /etc/pacman.d/mirrorlist
