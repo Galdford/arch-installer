72493e78be418e5746a0c2c53eec2568f4e3a97a
Galdford <galdford.verdugo@gmail.com>
2021-06-21 19:48:56 -0400

    Add files via upload

--- simplyarch.sh
+++ simplyarch.sh
@@ -179,9 +179,9 @@ then
 	# Install base system
 	if [[ -d /sys/firmware/efi ]]
 	then
-		pacstrap /mnt base base-devel linux-zen linux-firmware linux-zen-headers grub efibootmgr os-prober bash-completion sudo nano vim networkmanager ntfs-3g neofetch htop git reflector xdg-user-dirs e2fsprogs man-db intel-ucode boot-btrfs
+		pacstrap /mnt base base-devel linux-zen linux-firmware linux-zen-headers grub efibootmgr os-prober bash-completion sudo nano networkmanager ntfs-3g neofetch git reflector xdg-user-dirs e2fsprogs man-db intel-ucode boot-btrfs wpa_supplicant xf86-input-synaptics dhcpcd dialog
 	else
-		pacstrap /mnt base base-devel linux-zen linux-firmware linux-zen-headers grub os-prober bash-completion sudo nano vim networkmanager ntfs-3g neofetch htop git reflector xdg-user-dirs e2fsprogs man-db intel-ucode grub-btrfs
+		pacstrap /mnt base base-devel linux-zen linux-firmware linux-zen-headers grub os-prober bash-completion sudo nano networkmanager ntfs-3g neofetch git reflector xdg-user-dirs e2fsprogs man-db intel-ucode grub-btrfs wpa_supplicant xf86-input-synaptics dhcpcd dialog
 	fi
 	# fstab
 	genfstab -U /mnt >> /mnt/etc/fstab
